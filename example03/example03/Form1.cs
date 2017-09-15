d6e9c0bf61a988a1da994d79a8882e57975fa6cf
JongBaeRyu <gremian1013@gmail.com>
2017-09-15 23:09:00 +0900

    커밋

--- example03/example03/Form1.cs
+++ example03/example03/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,25 @@ namespace example03
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_seelctProcessFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "EXE File(*.exe)|*.exe|BAT File(*.bat)|*.bat";
+
+            if( openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.txt_processPath.Text = openFileDialog.FileName;
+
+                // exe or bat 파일을 프로세스로 등록
+                if( Path.GetExtension(openFileDialog.FileName) == ".exe")
+                {
+                    // 확장자 제거한 파일이름으로 프로세스 이름으로 등록               
+                    this.txt_processName.Text = Path.GetFileNameWithoutExtension(openFileDialog.SafeFileName);
+                }
+            }
         }
     }
 }
