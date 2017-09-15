f2de93c6dbbe2a7a7e9d90cdef9b082cb148ab68
daymisono <flower_op4@naver.com>
2017-09-16 00:25:02 +0900

    ggfdsgfd

---
+++ example03/example03/BLO/ProcessController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace example03.BLO
+{
+    using System;
+    using System.Threading;
+
+    class ProcessController
+    {
+        private ListView lst_processInfo;
+        private TextBox txt_log;
+
+        public ProcessController(ListView lst_processInfo, TextBox txt_log)
+        {
+            this.lst_processInfo = lst_processInfo;
+            this.txt_log = txt_log;
+        }
+
+        public void StartThread()
+        {
+            Thread th = new Thread(new ThreadStart(Execute));
+            th.Start();
+        }
+
+        public void Execute()
+        {
+            txt_log.AppendText("들어왔다.\n");
+        } 
+
+    }
+}
